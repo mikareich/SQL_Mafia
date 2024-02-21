d030317691c3f8d927c24a33591aa37aad2a225b
Mathis <mathishecktr@gmail.com>
2024-02-21 19:06:39 +0100

    Update story.js

--- story.js
+++ story.js
@@ -1,15 +1,15 @@
 export default [
   {
-    text: `Damals war deine Mafia die größte der Stadt, dann bist du ins Gefängnis gekommen. Jetzt bist du wieder draußen und musst die Stadt wiedererobern.`,
-    task: `Finde heraus, welche Gruppen sich gebildet haben.`,
+    text: "Nachdem du über 12 Jahre wegen eines bewaffneten Überfalls auf einen Politiker im Gefängnis saßt, ist heute der Tag, an welchem du deine Freiheit wiedererlagnst. Du gehst auf die Straße und bewunderst, wie sehr sich die Stadt seit deiner Verhaftung verändert hat. Dein treuster Untertante aus deiner alten Gang kommt dich abholen und hilft dir, dich zurecht zu finden.",
+    text:"Angelo: Wow Boss, du bist jetzt also auch mal aus'm Bau. Hat ja ewig gedauert! Du glaubst gar nicht, was hier bei uns alles passiert ist. Es haben haben sich echt viele neue Gangs gebildet und die Konkurrenz ist hart! ",
+    task: `Finde heraus, welche Gruppen es alles gibt.`,
     sqlRequest: `SELECT * FROM Gruppen`,
     sqlResult: [
       ["Name", "Mitglieder", "Territorium"],
       ["awdgziawgd", "1", "1"],
       ["Die Ratten", "10", "2"],
       ["Die Vögel", "5", "3"],
-    ],
-    answer: `Du gehörst zur Gruppe awdgziawgd.`
+    ]
   },
   {
     text: `Du hast herausgefunden, dass die Gruppe "Die Ratten" die Stadt kontrolliert. Du musst sie ausschalten.`,
@@ -17,5 +17,6 @@ export default [
     sqlRequest: `SELECT * FROM Gruppen WHERE name = "Die Ratten"`,
     sqlResult: ``,
     answer: `Die Ratten halten sich im Hafen auf.`,
+    y,
   },
 ];
