9448b94aa2599969324382b6febf4232787103fb
MarcoZillgen <77887301+MarcoZillgen@users.noreply.github.com>
2024-02-21 17:39:04 +0000

    Remove unnecessary tables and update answer in story.js

--- story.js
+++ story.js
@@ -9,31 +9,7 @@ export default [
       ["Die Ratten", "10", "2"],
       ["Die Vögel", "5", "3"],
     ],
-    answer: `Du gehörst zur Gruppe awdgziawgd.`,
-    groupTable: [
-      ["Name", "Mitglieder", "Territorium"],
-      ["awdgziawgd", "1", "1"],
-      ["Die Ratten", "10", "2"],
-      ["Die Vögel", "5", "3"],
-    ],
-    victimTable: [
-      ["Name", "Mitglieder", "Territorium"],
-      ["awdgziawgd", "1", "1"],
-      ["Die Ratten", "10", "2"],
-      ["Die Vögel", "5", "3"],
-    ],
-    itemTable: [
-      ["Name", "Mitglieder", "Territorium"],
-      ["awdgziawgd", "1", "1"],
-      ["Die Ratten", "10", "2"],
-      ["Die Vögel", "5", "3"],
-    ],
-    memberTable: [
-      ["Name", "Mitglieder", "Territorium"],
-      ["awdgziawgd", "1", "1"],
-      ["Die Ratten", "10", "2"],
-      ["Die Vögel", "5", "3"],
-    ],
+    answer: `Du gehörst zur Gruppe awdgziawgd.`
   },
   {
     text: `Du hast herausgefunden, dass die Gruppe "Die Ratten" die Stadt kontrolliert. Du musst sie ausschalten.`,
---
+++ tables.js
@@ -0,0 +1,26 @@
+export default {
+  groupTable: [
+    ["Name", "Mitglieder", "Territorium"],
+    ["awdgziawgd", "1", "1"],
+    ["Die Ratten", "10", "2"],
+    ["Die Vögel", "5", "3"],
+  ],
+  victimTable: [
+    ["Name", "Mitglieder", "Territorium"],
+    ["awdgziawgd", "1", "1"],
+    ["Die Ratten", "10", "2"],
+    ["Die Vögel", "5", "3"],
+  ],
+  itemTable: [
+    ["Name", "Mitglieder", "Territorium"],
+    ["awdgziawgd", "1", "1"],
+    ["Die Ratten", "10", "2"],
+    ["Die Vögel", "5", "3"],
+  ],
+  memberTable: [
+    ["Name", "Mitglieder", "Territorium"],
+    ["awdgziawgd", "1", "1"],
+    ["Die Ratten", "10", "2"],
+    ["Die Vögel", "5", "3"],
+  ],
+};
